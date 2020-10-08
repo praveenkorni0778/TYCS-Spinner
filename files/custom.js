1f4eccc8c2d643ba9d81cd4614d6e85651ad16c7
Praveen Korni <kornipraveen78@gmail.com>
2020-10-08 20:21:53 +0530

    Modifications for next workshop

--- files/custom.js
+++ files/custom.js
@@ -1,76 +1,104 @@
-const projectName = "random-quote-machine";
+const projectName = 'random-quote-machine';
 localStorage.setItem('example_project', 'Randowm Quote Machine');
 let quotesData;
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
-var numbers = [{"name":"ANTONITTA LORDUSWAMY","number":"1"},{"name":"ATULKUMAR MISHRA","number":"2"},{"name":"BHALERAO ROHAN","number":"3"},{"name":"BIRMOLE SAGAR","number":"4"},{"name":"CHANDAK HARSHAD","number":"5"},{"name":"CHAUDHARY AIJAZ","number":"6"},{"name":"GUPTA DHARMESH","number":"8"},{"name":"JADHAV VEDANGI","number":"9"},{"name":"JAISWAR BRIJESH","number":"10"},{"name":"KONAR NITESH","number":"11"},{"name":"MISHRA JAYENDRA","number":"13"},{"name":"NADAR PRIYA","number":"14"},{"name":"PAL SONU","number":"16"},{"name":"PANDEY RISHABH","number":"17"},{"name":"PARAB SAHIL","number":"18"},{"name":"PEDNEKAR SIDDHESH","number":"19"},{"name":"RAHATE OMKAR","number":"20"},{"name":"SAURABH PANDEY","number":"21"},{"name":"SAWANT PREET","number":"22"},{"name":"SHETTY NILESH","number":"24"},{"name":"SINGH SIMRAN","number":"25"},{"name":"NADAR SWEETY","number":"26"},{"name":"TAMBE SAHIL","number":"27"},{"name":"TIWARI RIDDHI","number":"28"},{"name":"VIPIN YADAV","number":"29"},{"name":"VISHWAKARMA VISHESHKUMAR","number":"30"},{"name":"WASHIMKAR AKASH","number":"31"},{"name":"YADAV AJAY","number":"32"},{"name":"YADAV PRADEEP","number":"33"}];
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-var currentQuote = ''; 
+function inIframe() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
+var numbers = [
+  { name: 'ATULKUMAR MISHRA', number: '2' },
+  { name: 'BIRMOLE SAGAR', number: '4' },
+  { name: 'CHAUDHARY AIJAZ', number: '6' },
+  { name: 'GUPTA DHARMESH', number: '8' },
+  { name: 'JAISWAR BRIJESH', number: '10' },
+  { name: 'KONAR NITESH', number: '11' },
+  { name: 'MISHRA JAYENDRA', number: '13' },
+  { name: 'NADAR PRIYA', number: '14' },
+  { name: 'PAL SONU', number: '16' },
+  { name: 'PARAB SAHIL', number: '18' },
+  { name: 'PEDNEKAR SIDDHESH', number: '19' },
+  { name: 'SAURABH PANDEY', number: '21' },
+  { name: 'SHETTY NILESH', number: '24' },
+  { name: 'TAMBE SAHIL', number: '27' },
+  { name: 'VIPIN YADAV', number: '29' },
+  { name: 'VISHWAKARMA VISHESHKUMAR', number: '30' },
+  { name: 'WASHIMKAR AKASH', number: '31' },
+  { name: 'YADAV AJAY', number: '32' },
+  { name: 'YADAV PRADEEP', number: '33' },
+];
+var colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857',
+];
+var currentQuote = '';
 
-function onSpin(){
-  $("#text").hide();
-  $("#text").text(" ");
-  $("#author").hide();
-  $("#author").text(" ");
-  $(".contain").show();
+function onSpin() {
+  $('#text').hide();
+  $('#text').text(' ');
+  $('#author').hide();
+  $('#author').text(' ');
+  $('.contain').show();
   $('audio#pop')[0].play();
-  setTimeout(function(){
+  setTimeout(function () {
     getQuote();
-  },10000);
+  }, 10000);
 }
 
 function getQuote() {
-  $(".contain").hide();
-  $("#text").show();
-  $("#author").show();
+  $('.contain').hide();
+  $('#text').show();
+  $('#author').show();
   let index = Math.floor(Math.random() * numbers.length);
   let randomQuote = numbers[index];
-  
-  numbers.splice(index,1);
-    
+
+  numbers.splice(index, 1);
+
   currentQuote = randomQuote.name;
   currentAuthor = randomQuote.number;
-  
-  $(".quote-text").animate(
-    { opacity: 0 },
-    500,
-    function() {
-      $(this).animate({ opacity: 1}, 500);
-      $('#text').text(randomQuote.name);
-    }
-  );
 
-  $(".quote-author").animate(
-    { opacity: 0 },
-    500,
-    function() {
-      $(this).animate({ opacity: 1}, 500);
-      $('#author').html("Roll no : "+randomQuote.number);
-    }
-  );
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#text').text(randomQuote.name);
+  });
+
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#author').html('Roll no : ' + randomQuote.number);
+  });
   $('audio#cheering')[0].play();
-  
+
   var color = Math.floor(Math.random() * colors.length);
   //
-  $("html body").css(
+  $('html body').css(
     {
       backgroundColor: colors[color],
-      color: colors[color]
+      color: colors[color],
     },
     1000
   );
   //
-  $(".button").css(
+  $('.button').css(
     {
-      backgroundColor: colors[color]
+      backgroundColor: colors[color],
     },
     1000
   );
 }
 
-$(document).ready(function() {
-  
-
+$(document).ready(function () {
   $('#new-quote').on('click', onSpin);
-
 });
